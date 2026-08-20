3d2ee7f4ee59a800df4aea6fb06933da53b54ca2
CC <ccano@cusdk12.org>
2026-08-20 01:08:39 +0000

    lab #5

---
+++ ccano5.py
@@ -0,0 +1,34 @@
+# Cesar Cano
+# Lab 5
+# Python Lists
+
+# list of 10 even numbers
+nums = [5, 15, 23, 20, 11, 6, 55, 2, 32, 25]
+
+print("No Loop")
+
+
+print(nums[0])
+print(nums[1])
+print(nums[2])
+print(nums[3])
+print(nums[4])
+print(nums[5])
+print(nums[6])
+print(nums[7])
+print(nums[8])
+print(nums[9])
+
+print("With Loop")
+
+# Print using a loop
+for i in nums:
+    print(i)
+
+
+# list of 5 fruits or vegetables
+foods = ["Strawberry", "Bananas", "Apples", "kale", "spinach"]
+
+# Print each element using the method shown in class
+for i in foods:
+    print(i)
