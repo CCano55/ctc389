83ade16eb61c13f5bd9b1278956e251ef8f2bef8
CC <ccano@cusdk12.org>
2026-09-10 00:13:55 +0000

    lab 1 2 review

---
+++ mid1.py
@@ -0,0 +1,10 @@
+# Cesar Cano
+# Python Program #1
+
+name = input("Enter your name: ")
+number = int(input("Enter a number: "))
+number2 = int(input("Pick a number to add to that number?: "))
+
+total = number + number2
+
+print(name, "you entered", number, "and added", number2, "to get a total of", total)
